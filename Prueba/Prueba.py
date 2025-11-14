f74f6cf98b531d37ded414576fec9ca91f26ff61
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-11-14 16:03:51 +0100

    prueba interface

--- Prueba/Prueba.py
+++ Prueba/Prueba.py
@@ -1,14 +1,31 @@
 import tkinter as tk
 
 root = tk.Tk()
-
+# título de la ventana */
 root.title("Mi primera ventana")
-
+# tamaño de la ventana */
 root.geometry("400x300")
+# evitar que la ventana se pueda redimensionar (width, height) */
+root.resizable(False, False)
 
+root.iconbitmap("icono.ico")
+
+
+
+# crear un label con el texto "¡Hola, Mundo!" */
 label = tk.Label(root, text="¡Hola, Mundo!")
-
+# mostrar el label en la ventana */
 label.pack()
 
+
+
+
+
+
+
+
+
+#la ventana tiene que estar a la escucha tiene que estar en un bucle infinito */
+#debe estar siempre al final del código /
 root.mainloop()
 
